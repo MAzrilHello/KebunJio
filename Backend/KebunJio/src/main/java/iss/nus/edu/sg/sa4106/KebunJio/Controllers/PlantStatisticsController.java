77f6c8d46233ec597b217a8753a39e87a73f52eb
WZY <ryanwang24.07.21@gmail.com>
2025-02-05 17:05:11 +0800

    new commit

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/PlantStatisticsController.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/PlantStatisticsController.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 @CrossOrigin(origins = "*")
 @Slf4j
 public class PlantStatisticsController {
-    @Autowired
     private final PlantStatisticsService plantStatisticsService;
 
     @GetMapping
