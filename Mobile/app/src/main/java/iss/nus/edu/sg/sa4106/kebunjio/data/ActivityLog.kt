ed85dc87324854b049746f42add2a4fe873c5d4f
marquan <chuahmark6@gmail.com>
2025-02-06 14:12:32 +0800

    Model for API communication with ActivityLog, Plant and EdiblePlantSpecies

--- Mobile/app/src/main/java/iss/nus/edu/sg/sa4106/kebunjio/data/ActivityLog.kt
+++ Mobile/app/src/main/java/iss/nus/edu/sg/sa4106/kebunjio/data/ActivityLog.kt
@@ -1,5 +1,7 @@
 package iss.nus.edu.sg.sa4106.kebunjio.data
 
+import java.io.Serializable
+
 data class ActivityLog(
     val id: String,
     val userId: String,
@@ -7,4 +9,4 @@ data class ActivityLog(
     val activityType: String,
     val activityDescription: String,
     val timestamp: String
-)
+) : Serializable
