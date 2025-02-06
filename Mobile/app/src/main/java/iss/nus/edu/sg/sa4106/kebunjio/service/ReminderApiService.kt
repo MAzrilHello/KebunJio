a0ee613e390a6bba90849973971bb2c0cf43e1e6
MAzrilHello <mazrilhello@gmail.com>
2025-02-07 07:58:04 +0800

    Web import fix and Mobile fixes

---
+++ Mobile/app/src/main/java/iss/nus/edu/sg/sa4106/kebunjio/service/ReminderApiService.kt
@@ -0,0 +1,12 @@
+package iss.nus.edu.sg.sa4106.kebunjio.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ReminderApiService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
