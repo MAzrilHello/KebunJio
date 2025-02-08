f51a5739b77ad39b1ae2a79c138ab06d31c85abe
YasmineK210 <yasminek210@gmail.com>
2025-02-08 09:38:52 +0800

    Resolving build issue

--- Web/kebunjio/src/features/user-profile/user-profile-page.js
+++ Web/kebunjio/src/features/user-profile/user-profile-page.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button'
 import Table  from "react-bootstrap/Table";
 import Appbar from '../../components/Appbar';
-import "./user-profile-new.css"
+import "./userProfileStyle.css"
 import Card from 'react-bootstrap/Card';
 
 const UserProfilePage = () => {
