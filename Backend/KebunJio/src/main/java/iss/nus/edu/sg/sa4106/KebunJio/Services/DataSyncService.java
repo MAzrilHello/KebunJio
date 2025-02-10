fcf7be0ac655a79d94ed74c03f6a78bf061459c8
ShiDu <1364136725@qq.com>
2025-02-10 16:40:18 +0800

    test

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Services/DataSyncService.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Services/DataSyncService.java
@@ -1,18 +1,18 @@
-package iss.nus.edu.sg.sa4106.KebunJio.Services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import iss.nus.edu.sg.sa4106.KebunJio.Models.Post;
-import iss.nus.edu.sg.sa4106.KebunJio.Models.PostES;
-import iss.nus.edu.sg.sa4106.KebunJio.Repository.PostESRepository;
-
-/* Todo:
- * 1. Use Elasticsearch API, map Post and PostIndexs Entity*/
-@Service
-public class DataSyncService {
-	@Autowired
-	private PostESRepository postESRepository;
-	
-
-}
+//package iss.nus.edu.sg.sa4106.KebunJio.Services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import iss.nus.edu.sg.sa4106.KebunJio.Models.Post;
+//import iss.nus.edu.sg.sa4106.KebunJio.Models.PostES;
+//import iss.nus.edu.sg.sa4106.KebunJio.Repository.PostESRepository;
+//
+///* Todo:
+// * 1. Use Elasticsearch API, map Post and PostIndexs Entity*/
+//@Service
+//public class DataSyncService {
+//	@Autowired
+//	private PostESRepository postESRepository;
+//	
+//
+//}
