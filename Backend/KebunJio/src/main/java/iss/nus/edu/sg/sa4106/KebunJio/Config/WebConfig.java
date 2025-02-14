4cb58ddfe40f6f3ce65aebd619814bd73ddccd8e
shidubei <71639835+shidubei@users.noreply.github.com>
2025-02-14 16:42:36 +0800

    Update WebConfig.java

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Config/WebConfig.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  
-                .allowedOrigins("http://34.124.209.141","http://localhost")  
+                .allowedOrigins("http://34.124.209.141","http://localhost:3000")  
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
                 .allowCredentials(true)
                 .allowedHeaders("*")  
