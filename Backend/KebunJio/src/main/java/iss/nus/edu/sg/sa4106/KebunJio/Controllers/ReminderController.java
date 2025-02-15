d460bd929ca58a55bb3d96c4cd0d0ffa093ef5d7
shidubei <71639835+shidubei@users.noreply.github.com>
2025-02-15 15:34:14 +0800

    Update ReminderController.java

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/ReminderController.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/ReminderController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Reminders")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class ReminderController {
 
     @Autowired
