84866a521eb5ff2ca570ee8a87aba7b4b39436d8
shidubei <71639835+shidubei@users.noreply.github.com>
2025-02-16 00:23:00 +0800

    Update PostService.java

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Services/PostService.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Services/PostService.java
@@ -124,7 +124,7 @@ public class PostService {
         postES.setPublishedDateTime(post.getPublishedDateTime());
         postES.setUserId(post.getUserId());
         postES.setPostCategory(post.getPostCategory());
-	postES.setUsename(post.getUsername());
+	postES.setUsername(post.getUsername());
         postESRepository.save(postES);
 
         return post;
