3a9198d5b674798e0947c268bed9ffc49f2191a1
shidubei <71639835+shidubei@users.noreply.github.com>
2025-02-15 15:33:08 +0800

    Update GoogleCalendarController.java

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/GoogleCalendarController.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/GoogleCalendarController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/google/calendar")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class GoogleCalendarController {
 
     @Autowired
