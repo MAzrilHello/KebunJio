6ae0185b883985c6fc45ec06c30ba6db5e53a0f8
marquan <chuahmark6@gmail.com>
2025-02-15 10:59:56 +0800

    Testing changing cd-pipeline.yml to see if can fix deployment issue

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/UserController.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/Controllers/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/users")
 // @CrossOrigin(origins = "http://34.124.209.141",allowCredentials = "true")
-public class UserController {
+public class UserController { // just to have some change
 
     @Autowired
     private UserService userService;
