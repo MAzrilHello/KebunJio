7dbae1213d4ccce2ec47dc60eedfc1aa87ecbd8d
YasmineK210 <yasminek210@gmail.com>
2025-02-21 21:28:18 +0800

    Update reply.js

--- Web/kebunjio/src/features/qna-forum/components/reply.js
+++ Web/kebunjio/src/features/qna-forum/components/reply.js
@@ -43,7 +43,7 @@ const Reply = ({userReply, hasLiked, hasDisliked, onDelete}) => {
         },{withCredentials:true})        
         .then(response=>{
             console.log(response)
-            setReply({ ...reply, content: editedContent });
+            setReply({ ...reply, commentContent: editedContent });
             setIsEditing(false);
         })
         .catch(err=>{
