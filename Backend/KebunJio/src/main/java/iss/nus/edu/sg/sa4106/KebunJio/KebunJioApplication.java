2474d6b23d64049df1518045caaf46c1fe461f21
MAzrilHello <mazrilhello@gmail.com>
2025-05-25 14:55:22 +0800

    Editted main spring code

--- Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/KebunJioApplication.java
+++ Backend/KebunJio/src/main/java/iss/nus/edu/sg/sa4106/KebunJio/KebunJioApplication.java
@@ -3,11 +3,21 @@ package iss.nus.edu.sg.sa4106.KebunJio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class KebunJioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KebunJioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(KebunJioApplication.class);
+        Map<String, Object> props = new HashMap<>();
+        String port = System.getenv("PORT");
+        if (port != null) {
+            props.put("server.port", port);
+        }
+        app.setDefaultProperties(props);
+        app.run(args);
+    }
 
 }
